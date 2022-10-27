7e1be7c05658e688453ad835c05257c4a1f7db0a
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-10-27 14:42:25 -0400

    added some buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
+import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+const names = ['Gustavo', 'Gerly', 'Cassandra', 'Corwin',
+'Jacques', 'Ainis', 'Gianna', 'Emily', 'Fanessa', 'Adi',
+'Mido', 'Cari', 'Eddie', 'Captain Ed', 'Todd', 'Nick',
+'Tyler', 'Brad'];
+
 function App() {
+
+  const [myName, setMyName] = useState("Eddie");
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {myName} is pretty cool.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          names.map(name => (
+            <button key={name} onClick={
+              ()=>setMyName(name)
+            }>{name}</button>
+          ))
+        }
+        <p>You clicked the button {counter} times.</p>
+        <button onClick={() => setCounter(counter + 1)}>+1</button>
+        <button onClick={() => setCounter(0)}>Reset</button>
+        <button onClick={
+          () => {
+            if (counter !== 0) {
+              setCounter(counter - 1)
+            }
+          }
+        }>-1</button>
       </header>
     </div>
   );
